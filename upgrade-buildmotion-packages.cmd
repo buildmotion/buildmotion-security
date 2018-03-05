80cce47f608d501ffd181130380294f1c9ec1d49
joe.gorilla <matt@hybridmobileapp.net>
2018-03-04 18:19:37 -0700

    buildmotion module updates;

---
+++ upgrade-buildmotion-packages.cmd
@@ -0,0 +1,2 @@
+REM use to upgrade buildmotion packages...yum.
+npm install angular-actions@latest angular-rules-engine@latest @buildmotion/logging@latest @buildmotion/foundation@latest @buildmotion/core@latest --save
